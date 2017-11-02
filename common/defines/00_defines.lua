76fd2bf5397a86d5c10da746df19769e5e3afd84
Yard1 <antoni.baum@gmail.com>
2017-11-02 16:45:58 +0100

    Added the generic magic tech folder Added texticons for Magical Infantry Some pony tech rebalancing

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -267,7 +267,7 @@ NTechnology = {
 	MAX_SUBTECHS = 3,						-- Max number of sub technologies a technology can have.
 	BASE_RESEARCH_POINTS_SAVED = 30.0,		-- Base amount of research points a country can save per slot.
 	BASE_YEAR_AHEAD_PENALTY_FACTOR = 2,		-- Base year ahead penalty
-	BASE_TECH_COST = 100,					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
+	BASE_TECH_COST = 96,					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
 	MAX_TECH_SHARING_BONUS = 0.5, 			-- Max technology sharing bonus that can be applied instantly
 	LICENSE_PRODUCTION_TECH_BONUS = 0.2,	-- License production tech bonus
 },
