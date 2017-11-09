8a5d70d79dda3a13a9cfc2046be29d7917fcd0c3
Antoni Baum <antoni.baum@gmail.com>
2017-11-09 22:35:22 +0100

    No need to have the whole defines file CHN more likely to research their techs Changed the Seer's college idea for OLE OLE shouldn't have issues with Johan disappearing when he shouldn't disappear Volksturmm tech will be prioritized if country has little manpower

---
+++ common/defines/eaw_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NGame.START_DATE = "1007.1.1.12",
+NDefines.NGame.END_DATE = "1022.1.1.1",
+
+NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1007.1.1.12",	-- Starting at this date, the tension values will be scaled down (will be equal to 1 before that)
+
+NDefines.NTechnology.BASE_TECH_COST = 96,					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
