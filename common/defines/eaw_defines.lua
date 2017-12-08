be69cea561ba6bdc2268eb05ef863920b8fcc967
Antoni Baum <antoni.baum@gmail.com>
2017-12-08 17:13:26 +0100

    Smarter AI Event localization and gfx Heinrich for GRF

--- common/defines/eaw_defines.lua
+++ common/defines/eaw_defines.lua
@@ -3,4 +3,4 @@ NDefines.NGame.END_DATE = "1022.1.1.1"
 
 NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1007.1.1.12"	-- Starting at this date, the tension values will be scaled down (will be equal to 1 before that)
 
-NDefines.NTechnology.BASE_TECH_COST = 94					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
+NDefines.NTechnology.BASE_TECH_COST = 90					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
