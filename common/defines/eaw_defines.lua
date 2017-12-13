7799bd65121651a8473b7823e1b258e6541c36be
Antoni Baum <antoni.baum@gmail.com>
2017-12-13 17:45:03 +0100

    Enhanced AI from Kr (we may need to ask them first)

--- common/defines/eaw_defines.lua
+++ common/defines/eaw_defines.lua
@@ -1,8 +1,229 @@
 NDefines.NGame.START_DATE = "1007.1.1.12"
 NDefines.NGame.END_DATE = "1030.1.1.1"
 
-NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1007.1.1.12"	-- Starting at this date, the tension values will be scaled down (will be equal to 1 before that)
+NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1007.1.1.12"
+NDefines.NTechnology.BASE_TECH_COST = 90
+NDefines.NAI.DYNAMIC_STRATEGIES_THREAT_FACTOR = 6.0
 
-NDefines.NTechnology.BASE_TECH_COST = 90					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
-NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 0.4;
-NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 1.0;
+-- Vanilla+ defines values
+
+--NDefines.NCountry.LOCAL_MANPOWER_ACCESSIBLE_NON_CORE_FACTOR = 0.05 		-- Vanilla is 0.02
+--NDefines.NCountry.MAJOR_MIN_FACTORIES = 50 								-- Vanilla is 35
+--NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2.5							-- Vanilla is 5
+--NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 100							-- Vanilla is 200
+--NDefines.NMilitary.FIELD_MARSHALL_CAP = 72								-- Vanilla is 0
+--NDefines.NMilitary.BASE_COMBAT_WIDTH = 96									-- Vanilla is 80
+--NDefines.NMilitary.ADDITIONAL_COMBAT_WIDTH = 48							-- Vanilla is 40
+--NDefines.NMilitary.PLAN_EXECUTE_CAREFUL_MAX_FORT = 3						-- Vanilla is 5
+--NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 0.75							-- Vanilla is 0.1
+--NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 1.0							-- Vanilla is 0.9
+--NDefines.NMilitary.DISBAND_MANPOWER_LOSS = 0.75							-- Vanilla is 0.7
+--NDefines.NMilitary.MIN_DIVISION_DEPLOYMENT_TRAINING = 0.5					-- Vanilla is 0.2
+--NDefines.NMilitary.FRONT_MIN_PATH_TO_REDEPLOY = 25						-- Vanilla is 8
+--NDefines.NAir.COMBAT_MAX_WINGS_AT_ONCE_PORT_STRIKE = 400					-- Vanilla is 10000
+--NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_DAMAGE_FACTOR = 15			-- Vanilla is 50
+--NDefines.NNavy.DETECTION_CHANCE_MULT_FORMATION_SPREAD_CONVOY_RAIDING = 1.6	-- Vanilla is 1.2
+--NDefines.NNavy.COMBAT_MIN_WIN_CHANCE_TO_KEEP_FIGHTING = 0.6				-- Vanilla is 0.2
+--NDefines.NNavy.COMBAT_CLOSE_DISTANCE_MAX_HIT_CHANCE_BONUS = 0.3			-- Vanilla is 0
+--NDefines.NNavy.COMBAT_DAMAGE_RANDOMNESS = 0.6								-- Vanilla is 0.3
+--NDefines.NNavy.COMBAT_DAMAGE_REDUCTION_ON_RETREAT = 0.5					-- Vanilla is 0.8
+--NDefines.NNavy.COMBAT_ESCAPING_SPEED_BALANCE = 0.7						-- Vanilla is 0.8
+--NDefines.NNavy.COMBAT_MAX_DISTANCE_TO_ARRIVE = 160						-- Vanilla is 80
+--NDefines.NNavy.COMBAT_MIN_DURATION = 18									-- Vanilla is 8
+--NDefines.NNavy.COMBAT_BASE_CRITICAL_CHANCE = 0.2							-- Vanilla is 0.1
+--NDefines.NNavy.REPAIR_AND_RETURN_PRIO_LOW = 0.4							-- Vanilla is 0.2
+--NDefines.NNavy.REPAIR_AND_RETURN_PRIO_MEDIUM = 0.6						-- Vanilla is 0.5
+--NDefines.NNavy.EXPERIENCE_FACTOR_CONVOY_ATTACK = 2						-- Vanilla is 0.05
+--NDefines.NNavy.EXPERIENCE_FACTOR_CARRIER_GAIN = 0.01						-- Vanilla is 0.1
+--NDefines.NNavy.NAVY_VISIBILITY_BONUS_ON_RETURN_FOR_REPAIR = 1.3			-- Vanilla is 0.9
+--NDefines.NNavy.NAVAL_SUPREMACY_INTEL_LOW = 0.5							-- Vanilla is 0.3
+--NDefines.NNavy.NAVAL_SUPREMACY_CAN_INVADE = 0.8							-- Vanilla is 0.5
+--NDefines.NNavy.SEA_AIR_COMBAT_MAX_WINGS_ON_STACK = 400					-- Vanilla is 200
+NDefines.NAI.DIPLOMACY_ACCEPT_VOLUNTEERS_BASE = 20							-- Vanilla is 50
+NDefines.NAI.RESEARCH_BONUS_FACTOR = 10.0									-- Vanilla is 0.9
+NDefines.NAI.NAVAL_DOCKYARDS_SHIP_FACTOR = 2.5								-- Vanilla is 1.5
+NDefines.NAI.NAVAL_BASES_SHIP_FACTOR = 1.25									-- Vanilla is 0.75
+NDefines.NAI.NAVAL_STATES_SHIP_FACTOR = 0.25								-- Vanilla is 0.75
+--NDefines.NAI.DEPLOY_MIN_TRAINING_PEACE_FACTOR = 1.0							-- Vanilla is 0.95
+--NDefines.NAI.DEPLOY_MIN_EQUIPMENT_PEACE_FACTOR = 1.0						-- Vanilla is 0.95
+--NDefines.NAI.DEPLOY_MIN_EQUIPMENT_WAR_FACTOR = 1.0							-- Vanilla is 0.90
+NDefines.NAI.LENDLEASE_FRACTION_OF_PRODUCTION = 0.25						-- Vanilla is 0.5
+--NDefines.NAI.PLAN_ATTACK_MIN_ORG_FACTOR_LOW = 0.9							-- Vanilla is 0.85
+--NDefines.NAI.PLAN_ATTACK_MIN_STRENGTH_FACTOR_LOW = 0.65						-- Vanilla is 0.4
+--NDefines.NAI.PLAN_ATTACK_MIN_ORG_FACTOR_MED = 0.7							-- Vanilla is 0.75
+--NDefines.NAI.PLAN_ATTACK_MIN_STRENGTH_FACTOR_MED = 0.45						-- Vanilla is 0.3
+--NDefines.NAI.PLAN_ATTACK_MIN_ORG_FACTOR_HIGH = 0.5							-- Vanilla is 0.2
+--NDefines.NAI.PLAN_ATTACK_MIN_STRENGTH_FACTOR_HIGH = 0.25					-- Vanilla is 0.15
+NDefines.NAI.RESERVE_TO_COMMITTED_BALANCE = 0.25							-- Vanilla is 0.1
+--NDefines.NAI.MAIN_ENEMY_FRONT_IMPORTANCE = 7.5								-- Vanilla is 4.0
+--NDefines.NAI.ORG_UNIT_STRONG = 0.8											-- Vanilla is 0.9
+--NDefines.NAI.ORG_UNIT_WEAK = 0.5											-- Vanilla is 0.15
+--NDefines.NAI.STR_UNIT_STRONG = 0.6											-- Vanilla is 0.75
+--NDefines.NAI.STR_UNIT_WEAK = 0.25											-- Vanilla is 0.2
+--NDefines.NAI.PLAN_FACTION_WEAK_TO_ABORT = 0.55								-- Vanilla is 0.65
+--NDefines.NAI.PLAN_AVG_PREPARATION_TO_EXECUTE = 0.6						-- Vanilla is 0.5
+--NDefines.NAI.AIR_WING_REINFORCEMENT_LIMIT = 300								-- Vanilla is 150
+--NDefines.NAI.UPGRADE_PERCENTAGE_OF_FORCES = 0.65							-- Vanilla is 0.1
+NDefines.NAI.NAVAL_MISSION_MIN_FLEET_SIZE = 5								-- Vanilla is 3
+--NDefines.NAI.NAVY_PREFERED_MAX_SIZE = 15									-- Vanilla is 20
+NDefines.NAI.MAX_DISTANCE_NALAV_INVASION = 300.0							-- Vanilla is 200
+NDefines.NAI.ENEMY_NAVY_STRENGTH_DONT_BOTHER = 1.25							-- Vanilla is 2.5
+NDefines.NAI.PRODUCTION_MAX_PROGRESS_TTO_SWITCH_NAVAL = 5.0					-- Vanilla is 0.3
+--NDefines.NAI.MAX_UNITS_FACTOR_AREA_ORDER = 1.5								-- Vanilla is 1.0
+--NDefines.NAI.DESIRED_UNITS_FACTOR_AREA_ORDER = 1.5							-- Vanilla is 1.0
+--NDefines.NAI.MAX_UNITS_FACTOR_FRONT_ORDER = 2.0								-- Vanilla is 1.5
+--NDefines.NAI.MAX_UNITS_FACTOR_INVASION_ORDER = 1.5							-- Vanilla is 1.0
+NDefines.NAI.FRONT_UNITS_CAP_FACTOR = 25.0									-- Vanilla is 15.0
+
+--
+-- EAI Defines values
+--
+
+NDefines.NAI.RESEARCH_NAVAL_DOCTRINE_NEED_GAIN_FACTOR = 2                       -- Multiplies value based on relative naval industry size / country size.
+NDefines.NAI.RESEARCH_AIR_DOCTRINE_NEED_GAIN_FACTOR = 2                         -- Multiplies value based on relative number of air base / country size.
+--NDefines.NAI.RESEARCH_DAYS_BETWEEN_WEIGHT_UPDATE = 1
+NDefines.NAI.MAX_AHEAD_RESEARCH_PENALTY = 10
+
+NDefines.NAI.RESEARCH_NEW_WEIGHT_FACTOR = 0			                            -- Impact of previously unexplored tech weights. Higher means more random exploration.
+NDefines.NAI.RESEARCH_BONUS_FACTOR = 2 				                            -- To which extent AI should care about bonuses to research
+NDefines.NAI.RESEARCH_AHEAD_OF_TIME_FACTOR = 0		                            -- To which extent AI should care about ahead of time penalties to research
+NDefines.NAI.RESEARCH_BASE_DAYS = 1					                            -- AI adds a base number of days when weighting completion time for techs to ensure it doesn't only research quick techs
+
+NDefines.NAI.DIVISION_UPGRADE_MIN_XP = 5							            -- Minimum XP before attempting to upgrade a division template.
+NDefines.NAI.DIVISION_CREATE_MIN_XP = 600							            -- Minimum XP before attempting to create a fresh new division template.
+NDefines.NAI.VARIANT_UPGRADE_MIN_XP = 400							            -- Minimum XP before attempting to create a new variant.
+NDefines.NAI.UPGRADE_XP_RUSH_UPDATE = 50								        -- If XP is above this on the daily tick the AI will attempt to spend it
+
+NDefines.NAI.UPGRADE_DIVISION_RELUCTANCE = 30
+NDefines.NAI.UPGRADE_PERCENTAGE_OF_FORCES = 0.1
+
+NDefines.NAI.ESTIMATED_CONVOYS_PER_DIVISION = 6
+
+NDefines.NAI.MAX_SUPPLY_DIVISOR = 0.5
+NDefines.NAI.MIN_SUPPLY_USE_SANITY_CAP = 100	                                -- Ignore supply cap if below this value when deciding on how many divisions to produce.
+
+NDefines.NAI.WAIT_YEARS_BEFORE_FREER_BUILDING = 1
+
+NDefines.NAI.WANTED_UNITS_INDUSTRY_FACTORY = 0 --0.0025 --0.001 		        -- How many units a country wants is partially based on how much military industry that is available
+
+NDefines.NAI.MIN_FIELD_STRENGTH_TO_BUILD_UNITS = 0.8			                -- Cancel unit production if below this to get resources out to units in the field
+NDefines.NAI.MIN_MANPOWER_TO_BUILD_UNITS = 0.0					                -- Cancel unit production if below this to get resources out to units in the field
+
+NDefines.NAI.PRODUCTION_EQUIPMENT_SURPLUS_FACTOR = 1.5
+
+--NDefines.NAI.TRADEABLE_FACTORIES_FRACTION = 1
+
+--NDefines.NAI.DEPLOY_MIN_TRAINING_PEACE_FACTOR = 0.9		                        -- Required percentage of training (1.0 = 100%) for AI to deploy unit in peacetime
+--NDefines.NAI.DEPLOY_MIN_TRAINING_WAR_FACTOR = 0.25		                        -- Required percentage of training (1.0 = 100%) for AI to deploy unit in wartime
+
+NDefines.NAI.START_TRAINING_EQUIPMENT_LEVEL = 0.9                               -- ai will not start to train if equipment drops below this level
+NDefines.NAI.STOP_TRAINING_EQUIPMENT_LEVEL = 0.8                                -- ai will not train if equipment drops below this level
+
+NDefines.NAI.NEW_LEADER_EXTRA_PP_FACTOR = 5.0
+
+NDefines.NAI.DIPLOMACY_IMPROVE_RELATION_COST_FACTOR = 7.0                       -- Desire to boost relations subtracts the cost multiplied by this
+
+NDefines.NAI.GENERATE_WARGOAL_THREAT_BASELINE = 0.6
+NDefines.NAI.DIPLOMACY_SEND_MAX_FACTION = 0.5
+
+NDefines.NAI.FALLBACK_LOSING_FACTOR = 0.0 					                    -- The lower this number  the longer the AI will hold the line before sending them to the fallback line
+
+NDefines.NAI.PLAN_FACTION_STRONG_TO_EXECUTE = 0.6 --0.80	0.60		        -- % or more of units in an order to consider ececuting the plan
+NDefines.NAI.ORG_UNIT_STRONG = 0.95 --0.5	 --0.75		0.9				            -- Organization % for unit to be considered strong
+NDefines.NAI.STR_UNIT_STRONG = 0.7 --0.9 --0.7		0.75					    -- Strength (equipment) % for unit to be considered strong
+
+NDefines.NAI.PLAN_FACTION_WEAK_TO_ABORT = 0.55 --0.50		0.65		        -- % or more of units in an order to consider ececuting the plan
+NDefines.NAI.ORG_UNIT_WEAK = 0.45 --0.25 --0.3			0.15					-- Organization % for unit to be considered weak
+NDefines.NAI.STR_UNIT_WEAK = 0.2 --0.6 --0.5			0.1					    -- Strength (equipment) % for unit to be considered weak
+
+--NDefines.NAI.AI_FRONT_MOVEMENT_FACTOR_FOR_READY = 0.4			                -- If less than this fraction of units on a front is moving  AI sees it as ready for action
+
+--NDefines.NAI.PLAN_ACTIVATION_MAJOR_WEIGHT_FACTOR = 0.0 		                    -- AI countries will hold on activating plans if stronger countries have plans in the same location. Majors count extra (value of 1 will negate this)
+
+NDefines.NAI.PLAN_MIN_SIZE_FOR_FALLBACK = 5000					                -- A country with less provinces than this will not draw fallback plans  but rather station their troops along the front
+
+NDefines.NAI.INVASION_COASTAL_PROVS_PER_ORDER = 15
+
+NDefines.NAI.NAVY_PREFERED_MAX_SIZE = 60						                -- AI will generally attempt to merge fleets into this size  but as a soft limit.
+
+NDefines.NAI.CONVOY_NEED_SAFETY_BUFFER = 40 --go crazy with it - control through scripts
+
+NDefines.NAI.AIR_WING_REINFORCEMENT_LIMIT = 100
+
+NDefines.NAI.AIR_MIN_ASSIGN_SCORE = 1 							                -- Minimum total score for region to be considered for air missions
+NDefines.NAI.AIR_MIN_REASSIGN_SCORE = 1 						                -- Minimum total score for region to be considered for reassigning air missions
+NDefines.NAI.AIR_WING_SIZE_FACTOR = 1000.0 						                -- Impact on air score of assigning wings to mission
+NDefines.NAI.AIR_MISSION_MIN_COVEREAGE = 0.2					                -- Between 0 and 1  limit for % coverage for air mission to be considered
+
+NDefines.NAI.LAND_DEFENSE_AIR_SUPERIORITY_IMPORTANCE = 1		                -- Strategic importance of air superiority ( amount of enemy planes in area )
+
+NDefines.NAI.LAND_DEFENSE_FIGHERS_PER_PLANE = 2 --1.1				            -- Amount of air superiority planes requested per enemy plane
+NDefines.NAI.LAND_DEFENSE_INTERSEPTORS_PER_BOMBERS = 2 --0.2		            -- Amount of air interceptor planes requested per enemy plane
+
+NDefines.NAI.LAND_COMBAT_AIR_SUPERIORITY_IMPORTANCE = 1	 --0.20		            -- Strategic importance of air superiority ( amount of enemy planes in area )
+NDefines.NAI.LAND_COMBAT_OUR_ARMIES_AIR_IMPORTANCE = 100 --12		            -- Strategic importance of our armies
+NDefines.NAI.LAND_COMBAT_OUR_COMBATS_AIR_IMPORTANCE = 100 --55		            -- Strategic importance of our armies in the combats
+NDefines.NAI.LAND_COMBAT_FRIEND_ARMIES_AIR_IMPORTANCE = 100 --12	            -- Strategic importance of friendly armies
+NDefines.NAI.LAND_COMBAT_FRIEND_COMBATS_AIR_IMPORTANCE = 100 --6		        -- Strategic importance of friendly armies in the combat
+
+NDefines.NAI.LAND_COMBAT_FIGHTERS_PER_PLANE = 2 --1.1				            -- Amount of air superiority planes requested per enemy plane
+NDefines.NAI.LAND_COMBAT_CAS_PER_ENEMY_ARMY = 100				                -- Amount of CAS planes requested per enemy army
+NDefines.NAI.LAND_COMBAT_BOMBERS_PER_LAND_FORT_LEVEL = 1 --15		            -- Amount of bomber planes requested per enemy land fort level
+NDefines.NAI.LAND_COMBAT_BOMBERS_PER_COASTAL_FORT_LEVEL = 1 --10	            -- Amount of bomber planes requested per enemy coastal fort level
+
+NDefines.NAI.STR_BOMB_MIN_ENEMY_FIGHTERS_IN_AREA = 100 --300		            -- If amount of enemy fighters is higher than this mission won't perform
+NDefines.NAI.STR_BOMB_FIGHTERS_PER_PLANE = 2 --0				                -- Amount of air superiority planes requested per enemy plane
+
+--
+-- Dickbong Defines values
+--
+
+NDefines.NDiplomacy.NOT_READY_FOR_WAR_BASE = -500					-- AI should be unwilling to enter accept a call to war if not ready for war against the relevant enemies.
+
+NDefines.NMilitary.PLAN_SPREAD_ATTACK_WEIGHT = 50.0				-- The higher the value, the less it should crowd provinces with multiple attacks.
+NDefines.NMilitary.PLAN_SUPPLY_FACTOR = 15.5						-- Factor applied to available supply on a front location when determining priority
+
+NDefines.NAI.GARRISON_FRACTION = 0.2					-- How large part of a front should always be holding the line rather than advancing at the enemy
+
+NDefines.NAI.DIPLOMACY_REJECTED_WAIT_MONTHS_BASE = 1	-- AI will not repeat offers until at least this time has passed, and at most the double
+
+NDefines.NAI.TRADEABLE_FACTORIES_FRACTION = 0.6			-- Will at most trade away this fraction of factories.
+
+NDefines.NAI.DEPLOY_MIN_TRAINING_PEACE_FACTOR = 0.95		-- Required percentage of training (1.0 = 100%) for AI to deploy unit in peacetime
+NDefines.NAI.DEPLOY_MIN_TRAINING_WAR_FACTOR = 0.05		-- Required percentage of training (1.0 = 100%) for AI to deploy unit in wartime
+NDefines.NAI.DEPLOY_MIN_EQUIPMENT_PEACE_FACTOR = 0.95	-- Required percentage of equipment (1.0 = 100%) for AI to deploy unit in peacetime
+NDefines.NAI.DEPLOY_MIN_EQUIPMENT_WAR_FACTOR = 0.90		-- Required percentage of equipment (1.0 = 100%) for AI to deploy unit in wartime
+
+NDefines.NAI.PLAN_VALUE_TO_EXECUTE = 0.2				-- AI will typically avoid carrying out a plan it below this value (0.0 is considered balanced).
+
+NDefines.NAI.SEND_VOLUNTEER_EVAL_BASE_DISTANCE = 0.0  -- How far away it will evaluate sending volunteers if not a major power
+NDefines.NAI.SEND_VOLUNTEER_EVAL_MAJOER_POWER = 0.0 	-- How willing major powers are to send volunteers.
+NDefines.NAI.SEND_VOLUNTEER_EVAL_CONTAINMENT_FACTOR = 0.9 -- How much AI containment factors into its evaluation of sending volunteers.
+
+NDefines.NAI.MAIN_ENEMY_FRONT_IMPORTANCE = 20.0			-- How much extra focus the AI should put on who it considers to be its current main enemy.
+NDefines.NAI.EASY_TARGET_FRONT_IMPORTANCE = 10.0			-- How much extra focus the AI should put on who it considers to be the easiest target.
+NDefines.NAI.AI_FRONT_MOVEMENT_FACTOR_FOR_READY = 0.4	-- If less than this fraction of units on a front is moving, AI sees it as ready for action
+
+NDefines.NAI.ENTRENCHMENT_WEIGHT = 4.0					-- AI should favour units with less entrenchment when assigning units around.
+
+NDefines.NAI.UNIT_ASSIGNMENT_TERRAIN_IMPORTANCE = 20.0	-- Terrain score for units are multiplied by this when the AI is deciding which front they should be assigned to
+
+NDefines.NAI.PLAN_ACTIVATION_MAJOR_WEIGHT_FACTOR = 1			-- AI countries will hold on activating plans if stronger countries have plans in the same location. Majors count extra (value of 1 will negate this)
+NDefines.NAI.PLAN_ACTIVATION_PLAYER_WEIGHT_FACTOR = 1		-- AI countries will hold on activating plans if player controlled countries have plans in the same location. Majors count extra (value of 1 will negate this)
+
+NDefines.NAI.NAVAL_MISSION_ESCORT_NEAR_CONTROLLED = 70000		-- Extra escort mission score near controlled provinces
+
+NDefines.NAI.HOUR_BAD_COMBAT_REEVALUATE = 48                 -- if we are in combat for this amount and it goes shitty then try skipping it
+
+NDefines.NAI.MAX_UNITS_FACTOR_AREA_ORDER = 1.0				-- Factor for max number of units to assign to area defense orders
+NDefines.NAI.DESIRED_UNITS_FACTOR_AREA_ORDER = 0.3				-- Factor for desired number of units to assign to area defense orders
+NDefines.NAI.MIN_UNITS_FACTOR_AREA_ORDER = 0.2					-- Factor for min number of units to assign to area defense orders
+
+NDefines.NAI.MAX_UNITS_FACTOR_FRONT_ORDER = 5.0					-- Factor for max number of units to assign to area front orders
+NDefines.NAI.DESIRED_UNITS_FACTOR_FRONT_ORDER = 4.5				-- Factor for desired number of units to assign to area front orders
+NDefines.NAI.MIN_UNITS_FACTOR_FRONT_ORDER = 3.0					-- Factor for min number of units to assign to area front orders
+NDefines.NAI.MAX_UNITS_FACTOR_INVASION_ORDER = 0.5				-- Factor for max number of units to assign to naval invasion orders
+NDefines.NAI.DESIRED_UNITS_FACTOR_INVASION_ORDER = 0.15			-- Factor for desired number of units to assign to naval invasion orders
+NDefines.NAI.MIN_UNITS_FACTOR_INVASION_ORDER = 0.1			-- Factor for min number of units to assign to naval invasion orders
+
+NDefines.NAI.PLAN_ACTIVATION_SUPERIORITY_AGGRO = 1.0			-- How aggressive a country is in activating a plan based on how superiour their force is.
