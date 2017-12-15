022fef506a7f09717689f89ba4b022f9a375afbf
Antoni Baum <antoni.baum@gmail.com>
2017-12-15 22:00:24 +0100

    Clean up defines from unused lines

--- common/defines/eaw_defines.lua
+++ common/defines/eaw_defines.lua
@@ -7,72 +7,17 @@ NDefines.NAI.DYNAMIC_STRATEGIES_THREAT_FACTOR = 6.0
 
 -- Vanilla+ defines values
 
---NDefines.NCountry.LOCAL_MANPOWER_ACCESSIBLE_NON_CORE_FACTOR = 0.05 		-- Vanilla is 0.02
---NDefines.NCountry.MAJOR_MIN_FACTORIES = 50 								-- Vanilla is 35
---NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2.5							-- Vanilla is 5
---NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 100							-- Vanilla is 200
---NDefines.NMilitary.FIELD_MARSHALL_CAP = 72								-- Vanilla is 0
---NDefines.NMilitary.BASE_COMBAT_WIDTH = 96									-- Vanilla is 80
---NDefines.NMilitary.ADDITIONAL_COMBAT_WIDTH = 48							-- Vanilla is 40
---NDefines.NMilitary.PLAN_EXECUTE_CAREFUL_MAX_FORT = 3						-- Vanilla is 5
---NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 0.75							-- Vanilla is 0.1
---NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 1.0							-- Vanilla is 0.9
---NDefines.NMilitary.DISBAND_MANPOWER_LOSS = 0.75							-- Vanilla is 0.7
---NDefines.NMilitary.MIN_DIVISION_DEPLOYMENT_TRAINING = 0.5					-- Vanilla is 0.2
---NDefines.NMilitary.FRONT_MIN_PATH_TO_REDEPLOY = 25						-- Vanilla is 8
---NDefines.NAir.COMBAT_MAX_WINGS_AT_ONCE_PORT_STRIKE = 400					-- Vanilla is 10000
---NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_DAMAGE_FACTOR = 15			-- Vanilla is 50
---NDefines.NNavy.DETECTION_CHANCE_MULT_FORMATION_SPREAD_CONVOY_RAIDING = 1.6	-- Vanilla is 1.2
---NDefines.NNavy.COMBAT_MIN_WIN_CHANCE_TO_KEEP_FIGHTING = 0.6				-- Vanilla is 0.2
---NDefines.NNavy.COMBAT_CLOSE_DISTANCE_MAX_HIT_CHANCE_BONUS = 0.3			-- Vanilla is 0
---NDefines.NNavy.COMBAT_DAMAGE_RANDOMNESS = 0.6								-- Vanilla is 0.3
---NDefines.NNavy.COMBAT_DAMAGE_REDUCTION_ON_RETREAT = 0.5					-- Vanilla is 0.8
---NDefines.NNavy.COMBAT_ESCAPING_SPEED_BALANCE = 0.7						-- Vanilla is 0.8
---NDefines.NNavy.COMBAT_MAX_DISTANCE_TO_ARRIVE = 160						-- Vanilla is 80
---NDefines.NNavy.COMBAT_MIN_DURATION = 18									-- Vanilla is 8
---NDefines.NNavy.COMBAT_BASE_CRITICAL_CHANCE = 0.2							-- Vanilla is 0.1
---NDefines.NNavy.REPAIR_AND_RETURN_PRIO_LOW = 0.4							-- Vanilla is 0.2
---NDefines.NNavy.REPAIR_AND_RETURN_PRIO_MEDIUM = 0.6						-- Vanilla is 0.5
---NDefines.NNavy.EXPERIENCE_FACTOR_CONVOY_ATTACK = 2						-- Vanilla is 0.05
---NDefines.NNavy.EXPERIENCE_FACTOR_CARRIER_GAIN = 0.01						-- Vanilla is 0.1
---NDefines.NNavy.NAVY_VISIBILITY_BONUS_ON_RETURN_FOR_REPAIR = 1.3			-- Vanilla is 0.9
---NDefines.NNavy.NAVAL_SUPREMACY_INTEL_LOW = 0.5							-- Vanilla is 0.3
---NDefines.NNavy.NAVAL_SUPREMACY_CAN_INVADE = 0.8							-- Vanilla is 0.5
---NDefines.NNavy.SEA_AIR_COMBAT_MAX_WINGS_ON_STACK = 400					-- Vanilla is 200
 NDefines.NAI.DIPLOMACY_ACCEPT_VOLUNTEERS_BASE = 20							-- Vanilla is 50
 NDefines.NAI.RESEARCH_BONUS_FACTOR = 10.0									-- Vanilla is 0.9
 NDefines.NAI.NAVAL_DOCKYARDS_SHIP_FACTOR = 2.5								-- Vanilla is 1.5
 NDefines.NAI.NAVAL_BASES_SHIP_FACTOR = 1.25									-- Vanilla is 0.75
 NDefines.NAI.NAVAL_STATES_SHIP_FACTOR = 0.25								-- Vanilla is 0.75
---NDefines.NAI.DEPLOY_MIN_TRAINING_PEACE_FACTOR = 1.0							-- Vanilla is 0.95
---NDefines.NAI.DEPLOY_MIN_EQUIPMENT_PEACE_FACTOR = 1.0						-- Vanilla is 0.95
---NDefines.NAI.DEPLOY_MIN_EQUIPMENT_WAR_FACTOR = 1.0							-- Vanilla is 0.90
 NDefines.NAI.LENDLEASE_FRACTION_OF_PRODUCTION = 0.25						-- Vanilla is 0.5
---NDefines.NAI.PLAN_ATTACK_MIN_ORG_FACTOR_LOW = 0.9							-- Vanilla is 0.85
---NDefines.NAI.PLAN_ATTACK_MIN_STRENGTH_FACTOR_LOW = 0.65						-- Vanilla is 0.4
---NDefines.NAI.PLAN_ATTACK_MIN_ORG_FACTOR_MED = 0.7							-- Vanilla is 0.75
---NDefines.NAI.PLAN_ATTACK_MIN_STRENGTH_FACTOR_MED = 0.45						-- Vanilla is 0.3
---NDefines.NAI.PLAN_ATTACK_MIN_ORG_FACTOR_HIGH = 0.5							-- Vanilla is 0.2
---NDefines.NAI.PLAN_ATTACK_MIN_STRENGTH_FACTOR_HIGH = 0.25					-- Vanilla is 0.15
 NDefines.NAI.RESERVE_TO_COMMITTED_BALANCE = 0.25							-- Vanilla is 0.1
---NDefines.NAI.MAIN_ENEMY_FRONT_IMPORTANCE = 7.5								-- Vanilla is 4.0
---NDefines.NAI.ORG_UNIT_STRONG = 0.8											-- Vanilla is 0.9
---NDefines.NAI.ORG_UNIT_WEAK = 0.5											-- Vanilla is 0.15
---NDefines.NAI.STR_UNIT_STRONG = 0.6											-- Vanilla is 0.75
---NDefines.NAI.STR_UNIT_WEAK = 0.25											-- Vanilla is 0.2
---NDefines.NAI.PLAN_FACTION_WEAK_TO_ABORT = 0.55								-- Vanilla is 0.65
---NDefines.NAI.PLAN_AVG_PREPARATION_TO_EXECUTE = 0.6						-- Vanilla is 0.5
---NDefines.NAI.AIR_WING_REINFORCEMENT_LIMIT = 300								-- Vanilla is 150
---NDefines.NAI.UPGRADE_PERCENTAGE_OF_FORCES = 0.65							-- Vanilla is 0.1
 NDefines.NAI.NAVAL_MISSION_MIN_FLEET_SIZE = 5								-- Vanilla is 3
---NDefines.NAI.NAVY_PREFERED_MAX_SIZE = 15									-- Vanilla is 20
 NDefines.NAI.MAX_DISTANCE_NALAV_INVASION = 300.0							-- Vanilla is 200
 NDefines.NAI.ENEMY_NAVY_STRENGTH_DONT_BOTHER = 1.25							-- Vanilla is 2.5
 NDefines.NAI.PRODUCTION_MAX_PROGRESS_TTO_SWITCH_NAVAL = 5.0					-- Vanilla is 0.3
---NDefines.NAI.MAX_UNITS_FACTOR_AREA_ORDER = 1.5								-- Vanilla is 1.0
---NDefines.NAI.DESIRED_UNITS_FACTOR_AREA_ORDER = 1.5							-- Vanilla is 1.0
---NDefines.NAI.MAX_UNITS_FACTOR_FRONT_ORDER = 2.0								-- Vanilla is 1.5
---NDefines.NAI.MAX_UNITS_FACTOR_INVASION_ORDER = 1.5							-- Vanilla is 1.0
 NDefines.NAI.FRONT_UNITS_CAP_FACTOR = 25.0									-- Vanilla is 15.0
 
 --
@@ -81,7 +26,6 @@ NDefines.NAI.FRONT_UNITS_CAP_FACTOR = 25.0									-- Vanilla is 15.0
 
 NDefines.NAI.RESEARCH_NAVAL_DOCTRINE_NEED_GAIN_FACTOR = 2                       -- Multiplies value based on relative naval industry size / country size.
 NDefines.NAI.RESEARCH_AIR_DOCTRINE_NEED_GAIN_FACTOR = 2                         -- Multiplies value based on relative number of air base / country size.
---NDefines.NAI.RESEARCH_DAYS_BETWEEN_WEIGHT_UPDATE = 1
 NDefines.NAI.MAX_AHEAD_RESEARCH_PENALTY = 10
 
 NDefines.NAI.RESEARCH_NEW_WEIGHT_FACTOR = 0			                            -- Impact of previously unexplored tech weights. Higher means more random exploration.
@@ -111,11 +55,6 @@ NDefines.NAI.MIN_MANPOWER_TO_BUILD_UNITS = 0.0					                -- Cancel uni
 
 NDefines.NAI.PRODUCTION_EQUIPMENT_SURPLUS_FACTOR = 1.5
 
---NDefines.NAI.TRADEABLE_FACTORIES_FRACTION = 1
-
---NDefines.NAI.DEPLOY_MIN_TRAINING_PEACE_FACTOR = 0.9		                        -- Required percentage of training (1.0 = 100%) for AI to deploy unit in peacetime
---NDefines.NAI.DEPLOY_MIN_TRAINING_WAR_FACTOR = 0.25		                        -- Required percentage of training (1.0 = 100%) for AI to deploy unit in wartime
-
 NDefines.NAI.START_TRAINING_EQUIPMENT_LEVEL = 0.9                               -- ai will not start to train if equipment drops below this level
 NDefines.NAI.STOP_TRAINING_EQUIPMENT_LEVEL = 0.8                                -- ai will not train if equipment drops below this level
 
@@ -135,10 +74,6 @@ NDefines.NAI.STR_UNIT_STRONG = 0.7 --0.9 --0.7		0.75					    -- Strength (equipm
 NDefines.NAI.PLAN_FACTION_WEAK_TO_ABORT = 0.55 --0.50		0.65		        -- % or more of units in an order to consider ececuting the plan
 NDefines.NAI.ORG_UNIT_WEAK = 0.45 --0.25 --0.3			0.15					-- Organization % for unit to be considered weak
 NDefines.NAI.STR_UNIT_WEAK = 0.2 --0.6 --0.5			0.1					    -- Strength (equipment) % for unit to be considered weak
-
---NDefines.NAI.AI_FRONT_MOVEMENT_FACTOR_FOR_READY = 0.4			                -- If less than this fraction of units on a front is moving  AI sees it as ready for action
-
---NDefines.NAI.PLAN_ACTIVATION_MAJOR_WEIGHT_FACTOR = 0.0 		                    -- AI countries will hold on activating plans if stronger countries have plans in the same location. Majors count extra (value of 1 will negate this)
 
 NDefines.NAI.PLAN_MIN_SIZE_FOR_FALLBACK = 5000					                -- A country with less provinces than this will not draw fallback plans  but rather station their troops along the front
 
