c4e1ac362b49da160748952a5bacee33f6d818c9
Antoni Baum <antoni.baum@protonmail.com>
2019-11-07 01:12:39 +0100

    Update PrepareForUpload.sh

--- PrepareForUpload.sh
+++ PrepareForUpload.sh
@@ -22,5 +22,5 @@ if [ -d $destinationName ] ; then
 fi
 
 mkdir $destinationName
-rsync --recursive -P  --exclude='*.7z' --exclude='MTG_notes.txt' --exclude='popularities_regex.txt' --exclude='.git*' --exclude='*.sh' --exclude='*.ps1' --exclude='*.psd' --exclude='*.py' --exclude='equestria_dev.mod' . $destinationName
+rsync --recursive -P  --exclude='*.7z' --exclude='regexes.txt' --exclude='MTG_notes.txt' --exclude='popularities_regex.txt' --exclude='.git*' --exclude='*.sh' --exclude='*.ps1' --exclude='*.psd' --exclude='*.py' --exclude='equestria_dev.mod' . $destinationName
 cp -f "$destinationName/equestria.mod" "$destinationName/.."
