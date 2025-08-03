5264b3eb7a26a47a74e50c5e874ea47991717eb3
JAPG <58125486+SpectrumVoid@users.noreply.github.com>
2025-08-03 11:49:59 -0400

    paperclip and cleanup

--- transfertechgen.py
+++ transfertechgen.py
@@ -19,7 +19,7 @@ import datetime
 ### The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 ### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ###
-### usage: hoi4transfertechsegen.py [-h] [-en effectname] [-a | -o] input output
+### usage: transfertechgen.py [-h] [-en effectname] [-a | -o] input output
 ###
 ### Given an input technology file or folder, generate a Transfer Technology
 ### scripted effect.
