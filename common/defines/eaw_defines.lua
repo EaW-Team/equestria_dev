11a10de1cf36ba32414166a2c406e3b241f75b75
GelberBaron <GelberBaron@users.noreply.github.com>
2026-09-11 12:49:50 +0200

    Minor fixes

--- common/defines/eaw_defines.lua
+++ common/defines/eaw_defines.lua
@@ -244,7 +244,6 @@ NDefines.NFactions.FACTION_INTELLIGENCE_ALLOWED_ADVISOR_TRAIT = {
 	"HYE_little_shadow",
 	"TBK_AF_lightbringer_advisor",
 	"TBK_AF_lightbringer_advisor_expanded",
-	"bother_builder",
 	"bother_builder_lar"
 }
 NDefines.NDoctrines.TRAINING_MASTERY_GAIN_FACTOR = 0.05 -- vanilla is 0.1
